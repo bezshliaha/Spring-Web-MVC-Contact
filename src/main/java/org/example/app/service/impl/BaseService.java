305856e1e75f65bf0a4c71a2fe5c5f18f3f6d758
bezshliaha <bezshliaha@gmail.com>
2024-05-19 17:56:53 +0200

    Initial commit

---
+++ src/main/java/org/example/app/service/impl/BaseService.java
@@ -0,0 +1,15 @@
+package org.example.app.service.impl;
+
+import java.util.List;
+
+public interface BaseService<T> {
+    boolean create(T obj);
+
+    List<T> fetchAll();
+
+    T fetchById(Long id);
+
+    boolean update(Long id, T obj);
+
+    boolean delete(Long id);
+}
